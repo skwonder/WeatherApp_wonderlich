5a3df743f95e7633810d83ec40d482b8c2346c27
skwonder <skwonder@bu.edu>
2018-04-20 14:11:01 -0400

    Upload
    Originanl Upload

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+// ========================================
+// Your JavaScript goes here
+// ========================================
+
+// If you want to use the toggleClass
+// function, uncomment the function below
+// and replace '.myClass' and
+// 'class-to-toggle' with your own data.
+// Both parameters must be in quotes.
+
+// toggleClass('.myClass', 'class-to-toggle')
+var now=new Date()
+console.log(now)
+
+var hours=now.getHours()
+console.log(hours)
+
+if(hours >=6 && hours <12){
+  console.log("morning")
+  document.body.classList.add('morning')
+}else if(hours >=12 && hours <=17){
+  console.log("afternoon")
+  document.body.classList.add('afternoon')
+}else{
+  console.log("night")
+  document.body.classList.add('night')
+  }
